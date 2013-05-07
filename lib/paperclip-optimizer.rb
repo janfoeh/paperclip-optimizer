73950ecf108b4279f9c6c131613cb62b3e83ed3e
Jan-Christian Foeh <jan@programmanstalt.de>
2013-05-07 18:04:27 +0200

    Capture and log ImageOptims verbose output
    Set verbose option to true by default.

--- lib/paperclip-optimizer.rb
+++ lib/paperclip-optimizer.rb
@@ -3,10 +3,29 @@ require "paperclip-optimizer/processor"
 
 module PaperclipOptimizer
   DEFAULT_SETTINGS = {
+    verbose: true,
     pngcrush: false, 
     pngout: false, 
     advpng: false, 
     jpegoptim: false, 
     gifsicle: false
   }.freeze
+
+  # Helper class for capturing ImageOptims error output and redirecting it
+  # to Paperclips logger instance
+  class StdErrCapture
+    def initialize(logger, log_level = :error)
+      @logger     = logger
+      @log_level  = log_level
+    end
+
+    def write(string)
+      @logger.send(@log_level, string)
+    end
+
+    alias_method "<<", :write
+
+    def flush; end
+  end
+
 end
--- lib/paperclip-optimizer/processor.rb
+++ lib/paperclip-optimizer/processor.rb
@@ -4,12 +4,23 @@ require "image_optim"
 module Paperclip
   class PaperclipOptimizer < Processor
     def make
-      src_path = File.expand_path(@file.path)
-
       settings = (@options[:paperclip_optimizer] || {}).reverse_merge(::PaperclipOptimizer::DEFAULT_SETTINGS)
 
-      image_optim           = ImageOptim.new(settings)
-      compressed_file_path  = image_optim.optimize_image(src_path)
+      src_path = File.expand_path(@file.path)
+
+      if settings[:verbose]
+        Paperclip.logger.info "optimizing #{src_path} with settings: #{settings.inspect}"
+
+        old_stderr  = $stderr
+        $stderr     = ::PaperclipOptimizer::StdErrCapture.new(Paperclip.logger)
+      end
+
+      begin
+        image_optim           = ImageOptim.new(settings)
+        compressed_file_path  = image_optim.optimize_image(src_path)
+      ensure
+        $stderr = old_stderr if settings[:verbose]
+      end
 
       if compressed_file_path && File.exists?(compressed_file_path)
         result = File.open(compressed_file_path)
